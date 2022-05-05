42ec7763d0c4183559c7b035199b413025408b03
steve <steve.nguyen2013@gmail.com>
2022-05-05 16:55:47 -0700

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  
+const form = document.querySelector("form")
+const tasks = document.querySelector("#tasks")
+
+
+form.addEventListener("submit",(e)=>{
+  e.preventDefault()
+ const inputValue = e.target.task.value
+ createToDo(inputValue)
+})
+
+
+
+function createToDo(element){
+  let p = document.createElement("p")
+  let button = document.createElement("button");
+  let priority = document.createElement('select');
+  let calander = document.createElement('tag');
+ 
+
+  p.textContent = `${element}`
+  button.textContent = " Finished ";
+priority.innerHTML = `
+<option>Priority</option>;
+<option>High</option>;
+<option>Medium</option>;
+<option>Low</option>;`;
+calander.innerHTML = `Date: <input type= "date">`;
+
+  button.addEventListener("click",(e)=>removeToDo(e))
+  
+
+  tasks.appendChild(p)
+  p.appendChild(priority)
+  p.appendChild(calander)
+  p.appendChild(button)
+  
+
+}
+function removeToDo(event){
+  event.target.parentNode.remove();
+}
+
+})
